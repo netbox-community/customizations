5079d1b13e3cae5da793a71ba93ba46674889d67
mmahacek <mmahacek@mcoe.org>
2019-10-03 14:28:41 -0700

    Update ip-reports.py
    Remove primary IP check report that has been split off to separate file

--- reports/ipam-reports/ip-reports.py
+++ reports/ipam-reports/ip-reports.py
@@ -14,41 +14,6 @@ LOOPBACK_ROLES = [
     IPADDRESS_ROLE_VRRP,
 ]
 
-class DeviceIPReport(Report):
-    description = "Check that every device has either an IPv4 or IPv6 primary address assigned"
-
-    def test_primary_ip4(self):
-        for device in Device.objects.filter(status=DEVICE_STATUS_ACTIVE):
-            intcount = 0
-            for interface in device.interfaces.all():
-                if not interface.mgmt_only:
-                    intcount += 1
-            # There may be dumb devices with no interfaces so no IP addresses, that's OK
-            if intcount == 0:
-                if device.primary_ip4_id is not None:
-                    if device.primary_ip6_id is not None:
-                        self.log_failure(device, "Device has primary IPv4 and IPv6 address but no interfaces")
-                    else:
-                        self.log_warning(device, "Device has missing primary IPv4 addresses but no interfaces")
-                else:
-                    self.log_success(device)
-            elif device.primary_ip4_id is None:
-                if device.device_type.is_child_device is True:
-                    self.log_success(device)
-                else:
-                    if device.primary_ip6_id is None:
-                        self.log_failure(device, "Device is missing primary IPv4 and IPv6 address")
-                    else:
-                        self.log_warning(device, "Device is missing primary IPv4 addresses")
-            else:
-                if device.device_type.is_child_device is True:
-                    self.log_success(device)
-                else:
-                    if device.primary_ip6_id is None:
-                        self.log_info(device, "Device is missing primary IPv6 address")
-                    else:
-                        self.log_success(device)
-
 # CheckPrimaryAddress reports forked from https://gist.github.com/candlerb/5380a7cdd03b60fbd02a664feb266d44
 class CheckPrimaryAddressDevice(Report):
     description = "Check that every device with an assigned IP has a primary IP address assigned"

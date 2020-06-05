4e5864ce868bab1d35ab957eb278c219c387cd6a
Ryan Merolle <ryanmerolle@users.noreply.github.com>
2020-06-05 10:03:50 -0400

    Add John's example report from NetBox Day 2020 (#32)
    * Modified NetBox Day 2020 report example by @lampwins

---
+++ reports/dcim-reports/CheckDeviceNaming.py
@@ -0,0 +1,19 @@
+import re
+
+from dcim.choices import DeviceStatusChoices
+from dcim.models import Device
+from extras.reports import Report
+
+# A modified John Anderson's NetBox Day 2020 Presentation by adding a check for all sites, not just LAX
+# All credit goes to @lampwins
+
+class DeviceHostnameReport(Report):
+   description = "Verify each device conforms to naming convention Example: spin-(site_name)-0001 or leaf-(site_name)-0001-a"
+
+   def test_device_naming(self):
+       for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
+           # Change the naming standard based on the re.match
+           if re.match("[a-z]{4}-" + str(device.site.name) + "-[0-9]{4}(-[a-b])?", str(device.name) , re.IGNORECASE):
+               self.log_success(device)
+           else:
+               self.log_failure(device, "Hostname does not conform to standard!")

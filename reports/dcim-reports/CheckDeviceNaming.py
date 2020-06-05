00e950ecabe1ed1549e6bcb17ddf8c465b97d489
Ryan Merolle <ryan.merolle@gmail.com>
2020-06-05 09:50:55 -0400

    fix def typo

--- reports/dcim-reports/CheckDeviceNaming.py
+++ reports/dcim-reports/CheckDeviceNaming.py
@@ -10,7 +10,7 @@ from extras.reports import Report
 class DeviceHostnameReport(Report):
    description = "Verify each device conforms to naming convention Example: spin-(site_name)-0001 or leaf-(site_name)-0001-a"
 
-   def test_devices_naming(self):
+   def test_device_naming(self):
        for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
            # Change the naming standard based on the re.match
            if re.match("[a-z]{4}-" + str(device.site.name) + "-[0-9]{4}(-[a-b])?", str(device.name) , re.IGNORECASE):

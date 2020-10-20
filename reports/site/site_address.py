2098bae7b698ccb2a6955f6e70c0b026d3dea5ea
mmahacek <mmahacek@mcoe.org>
2020-10-20 03:15:01 -0700

    Site Geocode Report/Script (#38)
    * Create geolocate_site.py
    
    Netbox script to identify sites that do not have latitude/longitude and run a geocode lookup based on the physical_address
    
    * Create site_address.py
    
    Check if site is missing address or geocode info

---
+++ reports/site/site_address.py
@@ -0,0 +1,31 @@
+# site_address.py
+
+# Make sure to add `geocoder` to your `local_requirements.txt` and make sure it is installed in your Python venv.
+
+import geocoder
+from extras.reports import Report
+from dcim.models import Site
+
+class checkSiteAddress(Report):
+    description = "Check if site has a physical address and/or geolocation information"
+
+    def test_site_address(self):
+        for site in Site.objects.all():
+            if site.physical_address:
+                self.log_success(site)
+            else:
+                self.log_failure(site, site.name)
+
+    def test_site_geo(self):
+        for site in Site.objects.all():
+            if site.latitude and site.longitude:
+                self.log_success(site)
+            else:
+                if site.physical_address:
+                    g = geocoder.osm(site.physical_address)
+                    if g:
+                        self.log_warning(site, f'Missing geo location - possible ({round(g.x,6)}, {round(g.y,6)})')
+                    else:
+                        self.log_warning(site, f'Missing geo location ({site.latitude}, {site.longitude})')    
+                else:    
+                    self.log_failure(site, f'Missing geo location ({site.latitude}, {site.longitude})')

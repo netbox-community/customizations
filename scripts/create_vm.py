8d177cc056e3b5e9273b8b3a69402ab4197085c2
Adrian Kus <47573296+adriankus@users.noreply.github.com>
2020-10-20 11:13:23 +0100

    Update create_vm.py (#41)
    Updated for Netbox 2.9

--- scripts/create_vm.py
+++ scripts/create_vm.py
@@ -1,7 +1,6 @@
 """
 This script allows you to create a VM, an interface and primary IP address
 all in one screen.
-
 Workaround for issues:
 https://github.com/netbox-community/netbox/issues/1492
 https://github.com/netbox-community/netbox/issues/648
@@ -14,8 +13,8 @@ from ipam.choices import IPAddressStatusChoices
 from ipam.models import IPAddress, VRF
 from tenancy.models import Tenant
 from virtualization.choices import VirtualMachineStatusChoices
-from virtualization.models import Cluster, VirtualMachine
-from extras.scripts import Script, StringVar, IPAddressWithMaskVar, ObjectVar, ChoiceVar, IntegerVar, TextVar
+from virtualization.models import Cluster, VirtualMachine, VMInterface
+from extras.scripts import Script, StringVar, IPAddressWithMaskVar, MultiObjectVar, ObjectVar, ChoiceVar, IntegerVar, TextVar
 
 class NewVM(Script):
     class Meta:
@@ -30,12 +29,12 @@ class NewVM(Script):
     dns_name = StringVar(label="DNS name", required=False)
     primary_ip4 = IPAddressWithMaskVar(label="IPv4 address")
     primary_ip6 = IPAddressWithMaskVar(label="IPv6 address", required=False)
-    #vrf = ObjectVar(VRF.objects, required=False)
-    role = ObjectVar(DeviceRole.objects.filter(vm_role=True), required=False)
+    #vrf = ObjectVar(model=VRF, required=False)
+    role = ObjectVar(model=DeviceRole, query_params={'vm_role':'True'})
     status = ChoiceVar(VirtualMachineStatusChoices, default=VirtualMachineStatusChoices.STATUS_ACTIVE)
-    cluster = ObjectVar(Cluster.objects)
-    #tenant = ObjectVar(Tenant.objects, required=False)
-    platform = ObjectVar(Platform.objects, required=False)
+    cluster = ObjectVar(model=Cluster)
+    tenant = ObjectVar(model=Tenant, required=False)
+    platform = ObjectVar(model=Platform)
     interface_name = StringVar(default="eth0")
     mac_address = StringVar(label="MAC address", required=False)
     vcpus = IntegerVar(label="VCPUs", required=False)
@@ -58,9 +57,8 @@ class NewVM(Script):
         )
         vm.save()
 
-        interface = Interface(
+        interface = VMInterface(
             name=data["interface_name"],
-            type=InterfaceTypeChoices.TYPE_VIRTUAL,
             mac_address=data["mac_address"],
             virtual_machine=vm,
         )
@@ -85,9 +83,9 @@ class NewVM(Script):
                 result = "Created"
             a.status = IPAddressStatusChoices.STATUS_ACTIVE
             a.dns_name = data["dns_name"]
-            if a.interface:
+            if result == "Assigned":
                 raise RuntimeError("Address %s is already assigned" % addr)
-            a.interface = interface
+            a.assigned_object = interface
             a.tenant = data.get("tenant")
             a.save()
             self.log_info("%s IP address %s %s" % (result, a.address, a.vrf or ""))
@@ -97,3 +95,4 @@ class NewVM(Script):
         add_addr(data["primary_ip6"], 6)
         vm.save()
         self.log_success("Created VM %s" % vm.name)
+        

cb945647d38e463d3f0af22a4915e53be1868138
Brian Candler <b.candler@pobox.com>
2021-02-18 22:43:52 +0000

    Update reports which use CustomFieldValue for Netbox 2.10 (#45)

--- reports/misc/CustomFieldValue.py
+++ reports/misc/CustomFieldValue.py
@@ -1,7 +1,6 @@
 from dcim.choices import DeviceStatusChoices
 from dcim.models import Device
 from extras.reports import Report
-from extras.models import CustomFieldValue, CustomField
 
 # This sample looks for a custom field named "Monitor" and then acts from there
 
@@ -9,13 +8,11 @@ class Check_IfMonitored(Report):
     description = "Check if device is flagged to be monitored"
 
     def test_monitoring_enabled(self):
-        for mf in CustomField.objects.filter(name='Monitor'):
-            MONITOR_FIELD = mf.id
         for device in Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE):
-            for monitor in CustomFieldValue.objects.filter(obj_id=device.id).filter(field_id=MONITOR_FIELD):
-                if monitor.value is True:
-                    self.log_success(device)
-                elif monitor.value is False:
-                    self.log_info(device, "Device set to not monitor")
-                else:
-                    self.log_warning(device, "Device has null monitoring field")
+            monitor = device.cf.get("Monitor")
+            if monitor is True:
+                self.log_success(device)
+            elif monitor is False:
+                self.log_info(device, "Device set to not monitor")
+            else:
+                self.log_warning(device, "Device has null monitoring field")

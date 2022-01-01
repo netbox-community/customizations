02aabf01dfadc9943d5750d29c70131b7866b641
Brian Candler <b.candler@pobox.com>
2022-01-01 14:41:27 +0000

    Add report and script for handling missing components on device from device type

---
+++ reports/dcim-reports/missing_device_type_components.py
@@ -0,0 +1,28 @@
+# Identify devices which are missing components from the device type definition
+
+from extras.reports import Report
+from dcim.models import Device
+
+class MissingDeviceTypeComponents(Report):
+    name = "Missing Device Type Components"
+    description = "Find devices which are missing components that are in the device type template"
+
+    def test_add_ports(self):
+        for device in Device.objects.all():
+            dt = device.device_type
+
+            for item, templateitem in [
+                ('consoleports', 'consoleporttemplates'),
+                ('consoleserverports', 'consoleserverporttemplates'),
+                ('powerports', 'powerporttemplates'),
+                ('poweroutlets', 'poweroutlettemplates'),
+                ('interfaces', 'interfacetemplates'),
+                ('rearports', 'rearporttemplates'),
+                ('frontports', 'frontporttemplates'),
+                ('devicebays', 'devicebaytemplates'),
+            ]:
+                names = {i.name for i in getattr(device, item).all()}
+                templatenames = {i.name for i in getattr(dt, templateitem).all()}
+                missing = templatenames - names
+                if missing:
+                    self.log_warning(device, "Missing %s %r" % (item, sorted(missing)))

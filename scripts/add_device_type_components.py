56b94b35d2e42e4e542a5c1729089ff314df3596
Brian Candler <b.candler@pobox.com>
2022-01-02 16:37:01 +0000

    Add report and script for handling missing components on device from device type (#51)

---
+++ scripts/add_device_type_components.py
@@ -0,0 +1,43 @@
+"""
+This script adds missing components from the device type to selected device(s)
+"""
+
+from dcim.models import Device, ConsolePort, ConsoleServerPort, PowerPort, PowerOutlet, Interface, RearPort, FrontPort, DeviceBay
+from extras.scripts import Script, MultiObjectVar
+
+class AddDeviceTypeComponents(Script):
+    class Meta:
+        name = "Add Device Type Components"
+        description = "Add missing components to selected devices"
+
+    devices = MultiObjectVar(
+        model=Device,
+    )
+
+    def run(self, data, commit):
+        for device in data["devices"]:
+            dt = device.device_type
+
+            # Based on Device.save():
+            # "If this is a new Device, instantiate all of the related components per the DeviceType definition"
+            # Note that ordering is important: e.g. PowerPort before PowerOutlet, RearPort before FrontPort
+            for klass, item, templateitem in [
+                (ConsolePort, 'consoleports', 'consoleporttemplates'),
+                (ConsoleServerPort, 'consoleserverports', 'consoleserverporttemplates'),
+                (PowerPort, 'powerports', 'powerporttemplates'),
+                (PowerOutlet, 'poweroutlets', 'poweroutlettemplates'),
+                (Interface, 'interfaces', 'interfacetemplates'),
+                (RearPort, 'rearports', 'rearporttemplates'),
+                (FrontPort, 'frontports', 'frontporttemplates'),
+                (DeviceBay,'devicebays', 'devicebaytemplates'),
+            ]:
+                names = {i.name for i in getattr(device, item).all()}
+                templates = getattr(dt, templateitem).all()
+                items = [
+                    x.instantiate(device)
+                    for x in templates
+                    if x.name not in names
+                ]
+                if items:
+                    klass.objects.bulk_create(items)
+                    self.log_success("%s (%d): created %d %s" % (device.name, device.id, len(items), item))

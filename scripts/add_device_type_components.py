20dc0358dcc4532e9155cd6f09c41576a86e1cb2
Misha Komarovskiy <zombah@gmail.com>
2022-04-24 12:34:41 +0300

    scripts: Add ModuleBay into type components script
    Netbox 3.2 inroduced new dcim model ModuleBay
    it is very useful to populate it with
    script to existing devices
    
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- scripts/add_device_type_components.py
+++ scripts/add_device_type_components.py
@@ -2,8 +2,11 @@
 This script adds missing components from the device type to selected device(s)
 """
 
-from dcim.models import Device, ConsolePort, ConsoleServerPort, PowerPort, PowerOutlet, Interface, RearPort, FrontPort, DeviceBay
+from dcim.models import (Device, ConsolePort, ConsoleServerPort, PowerPort,
+                         PowerOutlet, Interface, RearPort, FrontPort,
+                         DeviceBay, ModuleBay)
 from extras.scripts import Script, MultiObjectVar
+
 
 class AddDeviceTypeComponents(Script):
     class Meta:
@@ -19,17 +22,21 @@ class AddDeviceTypeComponents(Script):
             dt = device.device_type
 
             # Based on Device.save():
-            # "If this is a new Device, instantiate all of the related components per the DeviceType definition"
-            # Note that ordering is important: e.g. PowerPort before PowerOutlet, RearPort before FrontPort
+            # "If this is a new Device, instantiate all of the related
+            # components per the DeviceType definition"
+            # Note that ordering is important: e.g. PowerPort before
+            # PowerOutlet, RearPort before FrontPort
             for klass, item, templateitem in [
                 (ConsolePort, 'consoleports', 'consoleporttemplates'),
-                (ConsoleServerPort, 'consoleserverports', 'consoleserverporttemplates'),
+                (ConsoleServerPort, 'consoleserverports',
+                 'consoleserverporttemplates'),
                 (PowerPort, 'powerports', 'powerporttemplates'),
                 (PowerOutlet, 'poweroutlets', 'poweroutlettemplates'),
                 (Interface, 'interfaces', 'interfacetemplates'),
                 (RearPort, 'rearports', 'rearporttemplates'),
                 (FrontPort, 'frontports', 'frontporttemplates'),
-                (DeviceBay,'devicebays', 'devicebaytemplates'),
+                (DeviceBay, 'devicebays', 'devicebaytemplates'),
+                (ModuleBay, 'modulebays', 'modulebaytemplates'),
             ]:
                 names = {i.name for i in getattr(device, item).all()}
                 templates = getattr(dt, templateitem).all()
@@ -42,4 +49,7 @@ class AddDeviceTypeComponents(Script):
                     for i in items:
                         i.full_clean()
                     klass.objects.bulk_create(items)
-                    self.log_success("%s (%d): created %d %s" % (device.name, device.id, len(items), item))
+                    self.log_success("%s (%d): created %d %s" % (device.name,
+                                                                 device.id,
+                                                                 len(items),
+                                                                 item))

5ba5d6f2f4f1b5bd0847e83a90b60e6328567c87
Daniel Sheppard <dans@dansheps.com>
2023-08-28 14:09:35 -0500

    Update find_orphaned_cables.py

--- scripts/find_orphaned_cables.py
+++ scripts/find_orphaned_cables.py
@@ -6,24 +6,18 @@ from extras.scripts import Script
 
 class BrokenCableTerminations(Script):
     name = f'Find (partially) orphaned cables'
-    description = f'Find cable terminations that are orphaned or missing either the A or B termination'
+    description = f'Find cable terminations misising either the A or B termination'
 
     def run(self, data, commit):
-        cables = list(Cable.objects.filter(~Q(terminations__cable_end='A') & ~Q(terminations__cable_end='B')))
-        for end in ['A', 'B']:
-            if end == 'A':
-                negate = 'A'
-                include = 'B'
-            else:
-                negate = 'B'
-                include = 'A'
-            cable_side = Cable.objects.filter(~Q(terminations__cable_end=negate) & Q(terminations__cable_end=include))
-            cables.extend(list(cable_side))
-        self.log_info(f'Found {len(cables)} problematic cables')
+        cables = Cable.objects.annotate(
+            aterm=Count('terminations', filter=Q(terminations__cable_end="A")),
+            bterm=Count('terminations', filter=Q(terminations__cable_end="B")),
+        ).filter(Q(aterm__gte=1, bterm=0) | Q(aterm=0, bterm__gte=1) | Q(aterm=0, bterm=0))
+        self.log_info(f'Found {cables.count()} problematic cables in DB')
         for cable in cables:
-            if len(cable.a_terminations) == 0 and len(cable.b_terminations) > 0:
+            if cable.aterm == 0 and cable.bterm > 0:
                 self.log_warning(f'{cable.pk}: {cable} is missing \'A\' side termination')
-            elif len(cable.a_terminations) > 0 and len(cable.b_terminations) == 0:
+            elif cable.aterm > 0 and cable.bterm == 0:
                 self.log_warning(f'{cable.pk}: {cable} is missing \'B\' side termination')
             else:
                 self.log_warning(f'{cable.pk}: {cable} is orphaned')

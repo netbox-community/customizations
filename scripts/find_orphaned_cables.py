0c0b363a9967be051fde357d9b1418d6131917f5
Daniel Sheppard <dans@dansheps.com>
2024-02-13 10:58:10 -0600

    Apply suggestions from code review

--- scripts/find_orphaned_cables.py
+++ scripts/find_orphaned_cables.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Count, Q
 
 from dcim.models import Cable
 from extras.scripts import Script
@@ -12,12 +12,12 @@ class BrokenCableTerminations(Script):
         cables = Cable.objects.annotate(
             aterm=Count('terminations', filter=Q(terminations__cable_end="A")),
             bterm=Count('terminations', filter=Q(terminations__cable_end="B")),
-        ).filter(Q(aterm__gte=1, bterm=0) | Q(aterm=0, bterm__gte=1) | Q(aterm=0, bterm=0))
+        ).filter(Q(bterm=0) | Q(aterm=0) )
         self.log_info(f'Found {cables.count()} problematic cables in DB')
         for cable in cables:
             if cable.aterm == 0 and cable.bterm > 0:
-                self.log_warning(f'{cable.pk}: {cable} is missing \'A\' side termination')
+                self.log_warning(f'[{cable}](/dcim/cables/{cable.pk}/) is missing \'A\' side termination')
             elif cable.aterm > 0 and cable.bterm == 0:
-                self.log_warning(f'{cable.pk}: {cable} is missing \'B\' side termination')
+                self.log_warning(f'[{cable}](/dcim/cables/{cable.pk}/) is missing \'B\' side termination')
             else:
-                self.log_warning(f'{cable.pk}: {cable} is orphaned')
+                self.log_warning(f'[{cable}](/dcim/cables/{cable.pk}/) is orphaned')
